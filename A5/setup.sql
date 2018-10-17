aef384445c3e0bc382b9dc63cdc38d74db6ba9c1
Skinner <brian.skinner121@SIE-1119-06.local>
2018-10-16 23:48:39 -0700

    added template for load data local infile.

--- A5/setup.sql
+++ A5/setup.sql
@@ -8,15 +8,18 @@ create table input_file (
 	infile_path varchar(260) 
 );
 
+INSERT INTO input_file (infile_path)
+VALUES();
+
 drop table if exists nf1;
 create table nf1 ( 
 	order_id int,
 	line_number int,	
 	vendor_id int,
-	vendor_name varchar(),
+	vendor_name varchar(128),
 	product_id int,
-	product_name varchar(),
-	product_decription varchar(),
+	product_name varchar(128),
+	product_decription varchar(128),
 	product_price float,
 	order_quant float,
 	cost_per_order float,
@@ -25,3 +28,6 @@ create table nf1 (
 	arrival_date date
 );
 
+LOAD DATA LOCAL INFILE '<filename>'
+INTO TABLE nf1
+FIELDS TERMINATED BY ';' LINES terminated by '\r\n';
